92211f8557d96cbce819b565e1914f699d79af59
AlissonGRN <alissonnascimento.dev@gmail.com>
2025-04-17 19:30:47 -0300

    create poke api services

---
+++ lib/services/poke_api_service.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:pokedex/model/pokemon.dart';
+import 'package:pokedex/model/pokemon_details.dart';
+
+class PokeApiService {
+  static const String _baseUrl = 'https://pokeapi.co/api/v2';
+
+  Future<List<Pokemon>> fetchPokemons({int offset = 0}) async {
+    final response = await http.get(
+      Uri.parse('$_baseUrl/pokemon?offset=$offset&limit=20'),
+    );
+
+    if (response == 200) {
+      final data = json.decode(response.body);
+      return (data['results'] as List).map((p) => Pokemon.fromJson(p)).toList();
+    } else {
+      throw Exception("Failed to load pokemon");
+    }
+  }
+
+  Future<PokemonDetails> fetchPokemonDetails(int id) async {
+    final response = await http.get(Uri.parse('$_baseUrl/pokemon/$id'));
+
+    if (response.statusCode == 200) {
+      return PokemonDetails.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to load details');
+    }
+  }
+}
